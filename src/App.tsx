1f065970cddf8d1f19a965c8fcfcda5bbd1807ca
mrProger <ilya.kargapolov02@mail.ru>
2023-03-26 21:21:19 +0600

    Загружен шаблон проекта

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
