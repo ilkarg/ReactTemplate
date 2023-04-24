f5d016281a4c3625c663c917337cecdce427e980
Ilya <60040129+mrProger@users.noreply.github.com>
2023-04-24 10:13:54 +0600

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { Button } from "./components/Button";
-import { ButtonController } from "./controllers/Button";
+import { ButtonController } from "./controller/Button";
 
 const ControllerButton = ButtonController(5)
 
