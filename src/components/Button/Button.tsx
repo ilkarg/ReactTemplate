6e5c0c8f70654b586c1af4ce45bf01ee12ec28eb
Ilya_Kargapolov <ilya.kargapolov02@mail.ru>
2023-04-28 12:30:35 +0600

    Переделана структура шаблона

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
-import { ButtonModel } from "../../../Models";
+import { ButtonModel } from "../../Models";
+import "./styles/Button";
 
 export const Button = (props: ButtonModel) => {
     return (
